04c467011bd393eb40236f9de641c3a27ebc8a3d
RIZWAN <morizwan1410@gmail.com>
2025-09-15 20:05:35 +0530

    adding Player Movement script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody rb;
+    public Transform head;
+    public Camera camera;
+
+    [Header("Configurations")]
+    public float walkSpeed = 5.0f;
+    public float runSpeed = 10.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Horizontal rotation
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * 4f);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 newVelocity = Vector3.up * rb.velocity.y;
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        newVelocity.x = Input.GetAxis("Horizontal") * speed;
+        newVelocity.z = Input.GetAxis("Vertical") * speed;
+        rb.velocity = transform.TransformDirection(newVelocity);
+    }
+}
